512fa902597bd0dcf9f25f0cc388ee6eeaa28487
Omar Muy <muyof@farmingdale.edu>
2022-03-07 18:25:17 -0500

    Completed Step 1, making it a child of human and implementing all abstract methods etc.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,8 +9,23 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
+public class Student extends Human{
+    // ToDo 1: Make this class a child of Human
+    // To make it a child of human we must extend the Human class
+    public Student(String name, short age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAddress(String address) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+	
 	
 	// ToDo 2: Fix the resulting errors
 	
