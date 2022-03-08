2b97ae1a2748f31c37ccb37f65829e4d48483bb3
Omar Muy <muyof@farmingdale.edu>
2022-03-07 20:52:06 -0500

    Completed some steps, made GPA data, getters and setters included. Also made Senior and Freshman java applications

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -11,11 +11,23 @@ package com.mycompany.csc325_oop_designreview_lab;
  */
 public class Student extends Human{
     // ToDo 1: Make this class a child of Human
-    // To make it a child of human we must extend the Human class
+    // To make it a child of human we must extend the Human class and implement all abstract methods
+    // Also make for GPA and make getter and setters
+    private int gpa;
+    // ToDo 2: Fix the resulting errors
+    // ToDo 3: Add a field for GPA and create setter and getter
+    // ToDo 4: Add comments to your code
     public Student(String name, short age) {
         super(name, age);
     }
-
+    // GPA getter
+    public int getGPA() {
+        return gpa;
+    }
+    // GPA setter
+    public void setGPA(int gpa) {
+        this.gpa = gpa;
+    }
     @Override
     public String getAddress() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -26,11 +38,4 @@ public class Student extends Human{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 	
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
-
 }
